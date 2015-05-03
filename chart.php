1df4a648668af8c466d24b6f6d6b79d625090a43
8785496 <georg.88@mail.ru>
2015-05-03 13:09:16 +0600

    0.1.1

---
+++ chart.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>График</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!--<script src="http://api-maps.yandex.ru/2.1/?lang=ru_RU"></script>-->
+        <!--<script src="/view/js/map.js"></script>-->
+        <script src="http://d3js.org/d3.v3.min.js" charset="utf-8"></script>
+        <link rel="stylesheet" type="text/css" href="/view/css/style.css">
+        <link rel="stylesheet" type="text/css" href="/view/css/chart.css">
+    </head>
+    <body>
+        <script src="/view/js/chart.js"></script>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <?php
+
 //phpinfo();
 
-if(isset($_GET['x'])){
-    echo '<pre>';
-    print_r($_GET);
-    exit;
+if (isset($_GET['city'])) {
+    //$str = file_get_contents('data.json');
+    $arr = [
+        ["date" => "1-May-12", "close" => 582.13],
+        ["date" => "30-Apr-12", "close" => 583.98],
+        ["date" => "27-Apr-12", "close" => 603.00],
+        ["date" => "26-Apr-12", "close" => 607.70],
+        ["date" => "25-Apr-12", "close" => 750.00]
+    ];
+    $str = json_encode($arr);
+    echo $str;
+} else {
+    require 'view/default.php';
 }
-
-require 'view/default.php';
